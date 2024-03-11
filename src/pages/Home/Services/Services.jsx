402d0b0dba173765d533ef7d3ef4dae600ab85b6
QuaziSamiha <1703155@student.ruet.ac.bd>
2024-03-11 11:55:10 +0600

    google font added

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -28,11 +28,20 @@ const Services = () => {
             <h1 className="font-bold text-3xl pb-4">Services Offered</h1>
             <h3 className="text-lg font-semibold">What I Bring to the Table</h3>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            {console.log(services)}
-            <div className="w-56 h-44 p-16 m-4 rounded bg-green-800 bg-opacity-70">
-              <h1 className="text-3xl text-white font-thin">Hello World!</h1>
-            </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
+            {services.map((service, index) => (
+              <>
+                <div
+                  key={index}
+                  className="w-80 h-32 text-center p-3 m-4 rounded bg-[#263238] bg-opacity-70"
+                >
+                  <h1 className="text-xl text-white font-bold">
+                    {service.title}
+                  </h1>
+                  <p>{service.subtitle}</p>
+                </div>
+              </>
+            ))}
           </div>
         </div>
       </div>
